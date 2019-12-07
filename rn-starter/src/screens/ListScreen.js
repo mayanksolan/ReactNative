b14d62dc73d9cfd201910ed4d3989d91ae3a1633
Mayank Solan <mayanksolan@gmail.com>
2019-12-08 01:37:00 +0530

    ListScreen

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { view, Text, StyleSheet } from "react-native";
+import { view, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
-  return <Text>ListScreen</Text>;
+  const friends = [
+    { name: "Friend #1", key: "1" },
+    { name: "Friend #2", key: "2" },
+    { name: "Friend #3", key: "3" },
+    { name: "Friend #4", key: "4" },
+    { name: "Friend #5", key: "5" },
+    { name: "Friend #6", key: "6" },
+    { name: "Friend #7", key: "7" },
+    { name: "Friend #8", key: "8" },
+    { name: "Friend #9", key: "9" }
+  ];
+  return (
+    <FlatList
+      data={friends}
+      renderItem={({ item }) => {
+        return <Text style={styles.itemStyle}>{item.name}</Text>;
+      }}
+    />
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  itemStyle: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: "#d6d7da",
+    padding: 10,
+    fontSize: 20
+  }
+});
 
 export default ListScreen;
