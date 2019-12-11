e9f9a896c1cde4cbf55be1212bce789e3f93b115
Mayank Solan <mayanksolan@gmail.com>
2019-12-11 14:36:18 +0530

    done color

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -3,11 +3,52 @@ import { Text, StyleSheet, View, Button, FlatList } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+  const val = 20;
+
+  const setColor = (color, change) => {
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+      default:
+        return;
+    }
+  };
+
   return (
     <View>
-      <ColorCounter color="Red" />
-      <ColorCounter color="Blue" />
-      <ColorCounter color="Green" />
+      <ColorCounter
+        onIncrease={() => setColor("red", val)}
+        onDecrease={() => setColor("red", -1 * val)}
+        color="Red"
+      />
+      <ColorCounter
+        onIncrease={() => setColor("blue", +val)}
+        onDecrease={() => setColor("blue", -1 * val)}
+        color="Blue"
+      />
+      <ColorCounter
+        onIncrease={() => setColor("green", +val)}
+        onDecrease={() => setColor("green", -1 * val)}
+        color="Green"
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${green},${blue})`
+        }}
+      ></View>
     </View>
   );
 };
