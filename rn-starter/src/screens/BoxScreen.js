d27db82a1f2385d2bce0061dd86a5da404b9a00b
Mayank Solan <mayanksolan@gmail.com>
2019-12-22 13:36:59 +0530

    flex

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -2,9 +2,36 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const BoxScreen = () => {
-  return <Text>Box Screen</Text>;
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #22</Text>
+      <Text style={styles.textThreeStyle}>Child #333</Text>
+    </View>
+  );
 };
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: "black",
+    height: 200,
+    flexDirection: "row"
+  },
+  textOneStyle: {
+    borderWidth: 3,
+    borderColor: "orange",
+    flex: 4
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: "orange",
+    flex: 4
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: "orange",
+    flex: 2
+  }
+});
 
 export default BoxScreen;
